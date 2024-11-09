9d2e96787daba52554e0d2a36d7ea8239076f0e0
georgestaktak <georgestaktak110@gmail.com>
2024-11-09 13:34:05 +0200

    web

---
+++ javascriot.js
@@ -0,0 +1,13 @@
+let text=document.getElementById('text');
+let mybtn = document.getElementById('mybtn');
+
+
+window.onload= function(){
+text.value='hello'
+}
+
+
+
+mybtn.onclick = function(){
+    mybtn.style.background='red'
+}
